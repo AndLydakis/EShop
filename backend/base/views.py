b44e7e2a73140dd674413964fd091e13a571ac4f
AndLyd <andlydakis@gmail.com>
2021-02-23 21:40:22 +0000

    user register backend

--- backend/base/views.py
+++ backend/base/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from django.contrib.auth.models import User
+from django.contrib.auth.hashers import make_password
+
 from .models import Product
 from .products import products
 from .serializers import ProductSerializer, UserSerializer, UserSerializerWithToken
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework import permissions
 from rest_framework.response import Response
+from rest_framework import status
 
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
@@ -25,24 +28,22 @@ class MyTokenObtainPairView(TokenObtainPairView):
     serializer_class = MyTokenObtainPairSerializer
 
 
-# Create your views here.
-@api_view(['GET'])
-def getRoutes(request):
-    routes = [
-        '/api/products',
-        '/api/products/create',
+@api_view(['POST'])
+def registerUser(request):
+    data = request.data
 
-        '/api/products/upload',
-
-        '/api/products/<id>/reviews',
-
-        '/api/products/top',
-        '/api/products/<id>',
-
-        '/api/products/delete/<id>',
-        '/api/products/<update>/<id>',
-    ]
-    return Response(routes)
+    try:
+        user = User.objects.create(
+            first_name=data['name'],
+            username=data['email'],
+            email=data['email'],
+            password=make_password(data['password'])
+        )
+        serializer = UserSerializerWithToken(user, many=False)
+        return Response(serializer.data)
+    except Exception as e:
+        message = {'detail': 'user with this email already exists'}
+        return Response(message, status.HTTP_400_BAD_REQUEST)
 
 
 @api_view(['GET'])
