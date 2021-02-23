dca07011dd4fd2cf40d51d4cdd310f0ecd31eb63
AndLyd <andlydakis@gmail.com>
2021-02-23 21:25:17 +0000

    protected routes

--- backend/base/views.py
+++ backend/base/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from django.contrib.auth.models import User
 from .models import Product
 from .products import products
 from .serializers import ProductSerializer, UserSerializer, UserSerializerWithToken
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework import permissions
 from rest_framework.response import Response
 
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
@@ -58,8 +60,18 @@ def getProduct(request, pk):
 
 
 @api_view(['GET'])
+@permission_classes([permissions.IsAuthenticated])
 def getUserProfile(request):
     # this is token data not authenticated user object
     user = request.user
     serializer = UserSerializer(user, many=False)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+@permission_classes([permissions.IsAdminUser])
+def getUsers(request):
+    # this is token data not authenticated user object
+    users = User.objects.all()
+    serializer = UserSerializer(users, many=True)
     return Response(serializer.data)
