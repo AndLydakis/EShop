9c7c6584ec0046fca4a9ea4b5a517c2d17869402
AndLyd <andlydakis@gmail.com>
2021-02-25 20:25:32 +0000

    user login screen and functionality

--- backend/base/urls/user_urls.py
+++ backend/base/urls/user_urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from ..views import user_views as views
+from base.views import user_views as views
 
 
 urlpatterns = [
-    path('login', views.MyTokenObtainPairView.as_view(), name='login'),
-    path('register', views.registerUser, name='register'),
-    path('profile', views.getUserProfile, name='get_user_profile'),
+    path('login/', views.MyTokenObtainPairView.as_view(), name='login'),
+    path('register/', views.registerUser, name='register'),
+    path('profile/', views.getUserProfile, name='get_user_profile'),
     path('', views.getUsers, name='users'),
 ]
